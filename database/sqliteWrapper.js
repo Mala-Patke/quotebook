7fe0f958ed48b33f2a18a9e9c9b7d406cf45b675
Ali Shahid <hobocirclemeditation@gmail.com>
2021-09-29 15:02:22 -0700

    Finish wrapper for local database

--- database/sqliteWrapper.js
+++ database/sqliteWrapper.js
@@ -11,16 +11,18 @@ module.exports = class DBWrapper {
         "timestamp"
     ];
 
+    //Scripts
     static create = () => {
         db.prepare(`CREATE TABLE IF NOT EXISTS quotebook (
             quote TEXT NOT NULL,
             saidby TEXT NOT NULL,
             submittedby TEXT NOT NULL,
             timestamp BIGINT NOT NULL,
-            approved INT
+            approved INT DEFAULT 0
         )`).run();
     }
 
+    //Writes
     static insert = (quote, saidby, submby, timestamp) => {
         db.prepare(`
             INSERT INTO quotebook (quote, saidby, submittedby, timestamp, approved)
@@ -28,16 +30,54 @@ module.exports = class DBWrapper {
         `).run();
     }
 
+    //Reads
     /**
      * @private
-     * @param {object} params 
-     * @returns {string}
+     * @param {string} key
+     * @param {string} val 
+     * @returns {string[]}
      */
-    static _getQuotes = (key, val) => {
-        if(typeof(val) === 'string') val = `'${val}'`;
+    static _getQuotes = (key, val, comparator = '=') => {
+        if(typeof(val) === 'string'){
+            if(comparator === 'LIKE') val = `'%${val}%'`
+            else val = `'${val}'`;
+        } 
         
         return db.prepare(`
-            SELECT * FROM quotebook WHERE ${key} = ${val};
+            SELECT * FROM quotebook WHERE ${key} ${comparator} ${val}`)
+        .all();
+    }
+
+    static getQuotesByID = (id) => {
+        return this._getQuotes('rowid', id)
+    }
+
+    static getQuotesBySearchTerm = (quote) => {
+        return this._getQuotes('quote', quote, 'LIKE');
+    }
+
+    static getQuotesByAuthor = (author) => {
+        return this._getQuotes('saidby', author, 'LIKE');
+    }
+
+    static getQuotesBySubmitter = (submitter) => {
+        return this._getQuotes('submittedby', submitter, 'LIKE');
+    }
+
+    static getModQueue = () => {
+        return this._getQuotes('approved', 0, '=')
+    }
+
+    //Actually have to put in effort for this one
+    static getQuotesbyTimestamp = (lower, upper)=> {
+        return db.prepare(`
+            SELECT * FROM quotebook
+            WHERE timestamp > ${lower} AND timestamp < ${upper}
         `).all();
     }
 }
+
+/**
+ * Hitchcock uses the camera to follow the eyelines of the two observers.
+ * Additionally, the only sounds able to be heard are the loud yes
+ */
